f1566097730ea5b16590e78614e25f4b91601617
eddieware <47250727+eddieware@users.noreply.github.com>
2020-07-04 18:36:50 -0500

    update cards

--- lib/src/screens/action_sheet_screen.dart
+++ lib/src/screens/action_sheet_screen.dart
@@ -8,6 +8,7 @@ class ActionSheetScreen extends StatefulWidget {
 }
 
 class _ActionSheetScreenState extends State<ActionSheetScreen> {
+  String _selectedValue;
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -16,15 +17,92 @@ class _ActionSheetScreenState extends State<ActionSheetScreen> {
         previousPageTitle: 'Widgets',
       ),
       child: SafeArea(
-        child: Column(
-          children: <Widget>[
-            CupertinoButton.filled(
-              child: Text('Alert'),
-              onPressed: () {},
-            )
-          ],
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 50.0),
+              CupertinoButton.filled(
+                padding:
+                    EdgeInsets.symmetric(horizontal: 100.0, vertical: 10.0),
+                child: Text('Alert'),
+                onPressed: () => _showAlert(context),
+              ),
+              SizedBox(height: 16.0),
+              CupertinoButton.filled(
+                padding:
+                    EdgeInsets.symmetric(horizontal: 100.0, vertical: 10.0),
+                child: Text('Action Sheet'),
+                onPressed: () => _showAction(context),
+              ),
+              Text('Has presionado:$_selectedValue'),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  void _showAlert(BuildContext context) {
+    showCupertinoDialog<String>(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: Text('Guardar cambios?'),
+          content: Text('Desea guardar los cambios?'),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.pop(context, 'Cancelar'),
+            ),
+            CupertinoDialogAction(
+              child: Text('Aceptar'),
+              onPressed: () => Navigator.pop(context, 'Aceptar'),
+            )
+          ],
+        );
+      },
+    ).then((value) {
+      setState(() {
+        _selectedValue = value;
+      });
+    });
+  }
+
+  void _showAction(BuildContext context) {
+    showCupertinoModalPopup<String>(
+      context: context,
+      builder: (context) {
+        return CupertinoActionSheet(
+          title: Text('Framework Favorito'),
+          message: Text('Desea guardar los cambios?'),
+          actions: <Widget>[
+            CupertinoActionSheetAction(
+              child: Text('React Native'),
+              isDestructiveAction: true,
+              onPressed: () => Navigator.pop(context, 'React Native'),
+            ),
+            CupertinoActionSheetAction(
+              child: Text('Flutter'),
+              isDestructiveAction: true,
+              onPressed: () => Navigator.pop(context, 'Flutter'),
+            ),
+            CupertinoActionSheetAction(
+              child: Text('Native Scrip'),
+              isDestructiveAction: true,
+              onPressed: () => Navigator.pop(context, 'Native Scrip'),
+            ),
+          ],
+          cancelButton: CupertinoActionSheetAction(
+            child: Text('Cancelar'),
+            isDefaultAction: true,
+            onPressed: () => Navigator.pop(context, 'Cancelar'),
+          ),
+        );
+      },
+    ).then((value) {
+      setState(() {
+        _selectedValue = value;
+      });
+    });
   }
 }
--- lib/src/screens/card_screen.dart
+++ lib/src/screens/card_screen.dart
@@ -1,17 +1,94 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
-        middle: Text('Cupertino Cards'),
-      ),
-      child: Center(
-        child: Container(
-          child: Text('Hello Cards'),
+        child: CustomScrollView(
+      slivers: <Widget>[
+        CupertinoSliverNavigationBar(
+          largeTitle: Text('Cards'),
         ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+          _cardTipo1(),
+          _cardTipo2(context),
+        ]))
+      ],
+    ));
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      margin: EdgeInsets.all(20),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      elevation: 15,
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(CupertinoIcons.photo_camera),
+            title: Text('This is a card example'),
+            subtitle: Text(
+                'This is a subtitle lore   some text here to fill the space trying do not show the material imports '),
+          )
+        ],
       ),
+    );
+  }
+
+  Widget _cardTipo2(BuildContext contex) {
+    final _card = Stack(
+      alignment: Alignment.bottomCenter,
+      children: <Widget>[
+        FadeInImage(
+          height: 300,
+          placeholder: AssetImage('assets/cupertino_activity_indicator.gif'),
+          image:
+              NetworkImage('https://jooinn.com/images/perfect-landscape-1.jpg'),
+          fit: BoxFit.cover,
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
+        Opacity(
+          opacity: .6,
+          child: Container(
+            height: 80.0,
+            width: double.infinity,
+            color: CupertinoColors.black,
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  '2020-07-04',
+                  style: TextStyle(color: CupertinoColors.white),
+                ),
+                Text(
+                  'Los Azufres Michoacán',
+                  style:
+                      TextStyle(color: CupertinoColors.white, fontSize: 25.0),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+
+    return Container(
+      //height: 100.0,
+      margin: EdgeInsets.all(20.0),
+      decoration: BoxDecoration(
+        //color: CupertinoColors.activeBlue,
+        borderRadius: BorderRadius.circular(10.0),
+        boxShadow: [
+          BoxShadow(
+              color: CupertinoColors.darkBackgroundGray.withOpacity(.7),
+              offset: Offset(0.0, 5.0),
+              blurRadius: 8.0)
+        ],
+      ),
+      child: ClipRRect(borderRadius: BorderRadius.circular(10.0), child: _card),
     );
   }
 }
