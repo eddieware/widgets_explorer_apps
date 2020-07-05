32b1088eeaf0077ad83fdbd5b1964cfbb408f493
eddieware <47250727+eddieware@users.noreply.github.com>
2020-07-05 18:33:37 -0500

    updated segmented

--- lib/src/screens/segmented_control.dart
+++ lib/src/screens/segmented_control.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SegmentedControl extends StatefulWidget {
   SegmentedControl({Key key}) : super(key: key);
@@ -9,6 +10,22 @@ class SegmentedControl extends StatefulWidget {
 
 class _SegmentedControlState extends State<SegmentedControl> {
   @override
+  int defaultValue = 0;
+  final Map<int, Widget> mapaOpciones = const <int, Widget>{
+    0: Text(
+      "Indigo",
+      style: TextStyle(color: CupertinoColors.black),
+    ),
+    1: Text(
+      "Teal",
+      style: TextStyle(color: CupertinoColors.black),
+    ),
+    2: Text(
+      "Cyan",
+      style: TextStyle(color: CupertinoColors.black),
+    )
+  };
+
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
@@ -18,8 +35,49 @@ class _SegmentedControlState extends State<SegmentedControl> {
         child: SafeArea(
             child: Center(
           child: Column(
-            children: <Widget>[Text('SEGEMENTED')],
+            children: <Widget>[
+              Container(
+                //alignment: Alignment.bottomLeft,
+                //color: CupertinoColors.activeBlue,
+                padding: EdgeInsets.symmetric(vertical: 20),
+                width: 400,
+                child: Expanded(
+                  child: Container(
+                    child: CupertinoSegmentedControl(
+                      unselectedColor: CupertinoColors.systemGrey5,
+                      selectedColor: CupertinoColors.white,
+                      borderColor: CupertinoColors.systemGrey3,
+                      groupValue: defaultValue,
+                      onValueChanged: (changedValue) {
+                        setState(() {
+                          defaultValue = changedValue;
+                        });
+                        print(defaultValue);
+                      },
+                      children: mapaOpciones,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 50),
+              Container(
+                //color: CupertinoColors.activeBlue,
+                padding: EdgeInsets.all(50),
+                child: _cambiaColor(),
+              )
+            ],
           ),
         )));
+  }
+
+  _cambiaColor() {
+    if (defaultValue == 0) {
+      return FlutterLogo(size: 200, colors: Colors.indigo);
+    } else if (defaultValue == 1) {
+      return FlutterLogo(size: 200, colors: Colors.teal);
+    } else {
+      return FlutterLogo(size: 200, colors: Colors.cyan);
+    }
+    //
   }
 }
